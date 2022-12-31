a3728f152b15d1f72029eb197807bf8f3b02a34f
thakoraarya <aarya191102@gmail.com>
2022-12-31 16:18:46 +0530

    getting user

--- src/App.js
+++ src/App.js
@@ -1,32 +1,39 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import './App.css';
 import Login from './components/Login';
 import { getTokenFromUrl } from "./Spotify";
-// import {
-//   BrowserRouter as Router,
-//   Routes,
-//   Route,
-// } from "react-router-dom";
+import SpotifyWebApi from 'spotify-web-api-js';
 function App() {
+// --------------- V ---------------- here i forgot to add the word ""new"" before SpotifyWebApi and it take my 2hours to find the error
+  const spotify = new SpotifyWebApi();
 
-useEffect(()=>{
-const token = getTokenFromUrl();
-console.log("my token is : ", token);
-},[]);
+  const [token, setToken] = useState(null);
+
+  useEffect(() => {
+    const hash = getTokenFromUrl();
+    window.location.hash = "";
+
+    const _token = hash.access_token;
+
+    if (_token) {
+      setToken(_token);
+
+      spotify.setAccessToken(_token);
+
+      spotify.getMe().then(user => {
+        console.log(user);
+      })
+    }
+    console.log("my token is : ", token);
+  }, []);
 
 
   return (
-   <div>
-    {/* <Router>
-          <Routes>
-            <Route exact path="/" element={<Login/>} />
-            <Route exact path="/login" element={<Login/>} />
-           </Routes>
-        </Router> */}
-<Login/>
+    <div>{
+      token ? console.log("logged in") : <Login />
+    }
 
-
-   </div>
+    </div>
 
   );
 }
